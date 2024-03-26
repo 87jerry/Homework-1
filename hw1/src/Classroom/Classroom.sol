70ca66f9fa65d11881f8eba362bd9f54fae8649e
87jerry <161699506+87jerry@users.noreply.github.com>
2024-03-27 01:32:52 +0800

    Add files via upload

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,17 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+	bool private isEnrolled;
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+		if(isEnrolled)
+			return 123;
+		else
+		{
+			isEnrolled = true;
+			return 1000;
+		}
+			
     }
 }
 
@@ -18,12 +26,23 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+		if(IClassroomV2(msg.sender).isEnrolled())
+			return 123;
+		else
+			return 1000;
+		
     }
 }
 
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
+	error gas_left(uint256);
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+		//revert gas_left(gasleft());
+		if(gasleft()<7000)
+			return 123;
+		else
+			return 1000;
     }
 }
